0302e6f1a6d37925c1b8f1907ee373f4b2083044
omaar-es <omar.redondo.gonzalez.1b@gmail.com>
2024-04-03 00:10:24 -0600

    Cambios en windows para leer un archivo de texto

--- Pract2_Tokens/token.c
+++ Pract2_Tokens/token.c
@@ -19,59 +19,47 @@ typedef struct Token{
 manejaError: En caso de presentarse errores en la asignación de memoria 
 introducirToken: Tipo de token, valor, se acumula el valor total de tokens
 */
+void insertarFilaEnPila(FILE *, Pila *);
+void IntercambiarPilas(Pila *, Pila *);
 void manejaError(int);
 void introducirToken(char *, char *, Token *, int *);
 
 void main(int argc, char *argv[]){
   //Creamos variables inciales con espacio de 1 para la cadena
   char *input = NULL, cadenaTmp[100];
-  int longitud, numTokens, i, noPruebas = 0;
+  int longitud, numTokens, i;
   char caracter;
   PILA Stack1, Stack2;
   Token *listTokens = NULL;
-
+  FILE *archivo;
   //Verificamos que haya introducido el numero de cadenas a evaluar
   if(argc != 2){
     printf("Ejecucion incorrecta. Debe de proporcionar el numero de cadenas a probar.\n");
     exit(0);
   }
 
-  noPruebas = atoi(argv[1]);
+  archivo=fopen(argv[1], "r");
+  if(archivo==NULL){
+      printf("\n El archivo no se pudo abrir");
+      fclose(archivo);
+      return 1;
+  }
 
-  while(noPruebas--){
+  while(feof(archivo)==0){
     longitud = 1, numTokens = 0;
-    printf("Introduzca la formula a analizar: ");
-    //Recolectamos la cadena caracter por cracter usando memoria dinamica
-    while((caracter = getchar()) != '\n' && caracter != EOF){
-      input = (char*)realloc(input, longitud * sizeof(char));
-      if(input == NULL){
-        printf("Error al asignar memoria a la cadena de lectura");
-        exit(0);
-      }
-      //Almacenamos y aseguramos que el último será siempre el caracter nulo
-      input[longitud - 1] = caracter;
-      input[longitud] = '\0';
-      longitud++;
-    }
-
-    //Restamos uno a la longitud ya que al final se tiene el caracter nulo
-    longitud--;
-    printf("\nDe la formula: '%s' sus tokens son:\n\n", input);
     Stack1 = crearPila();
     Stack2 = crearPila();
-
     //Apilamos, pero el TAD Pila es LIFO (se almacenará al revés)
-    for (i = 0; i < longitud; i++){
-      apilar(Stack1, input[i]);
-    }
-
+    insertarFilaEnPila(archivo, Stack1);
     //Usamos Stack2 para reordenar
-    while(!es_vaciaPila(Stack1)) {
-      apilar(Stack2, desapilar(Stack1));
-    } 
+    IntercambiarPilas(Stack1, Stack2);
+    input[longitud] = '\0';
+    //Restamos uno a la longitud ya que al final se tiene el caracter nulo
+    //longitud--;
+    //printf("\nDe la formula: '%s' sus tokens son:\n\n", input);
     
     //Analizamos todo, para ello creamos el arreglo de tokens que se piensa tener uno por cada caracter en el peores de los casos
-    listTokens = malloc(longitud * sizeof(Token));
+    listTokens = (Token*)malloc(longitud * sizeof(Token));
     while (!es_vaciaPila(Stack2)){
       i = 0;
       cadenaTmp[i] = '\0';
@@ -143,7 +131,7 @@ void main(int argc, char *argv[]){
 
 void introducirToken(char *tipoToken, char *cadenaTmp, Token *listTokens, int *numTokens){
   strcpy(listTokens[(*numTokens)].id, tipoToken);
-  listTokens[(*numTokens)].valor = malloc(strlen(cadenaTmp) * sizeof(char));
+  listTokens[(*numTokens)].valor = (char*)malloc(strlen(cadenaTmp) * sizeof(char));
   strcpy(listTokens[(*numTokens)].valor, cadenaTmp);
   (*numTokens)++;
 }
@@ -151,4 +139,32 @@ void introducirToken(char *tipoToken, char *cadenaTmp, Token *listTokens, int *n
 void manejaError(int opcion){
 	char *mensaje[] = {"No hay memoria disponible", "Pila vacia", "Liberando memoria", "Saliendo del programa", "Estacionamiento lleno"};
 	printf("\n%s\n", mensaje[opcion]);
+}
+
+//Inserta cierta fila de un archivo de texto en una pila y devuelve la cantidad
+//de elementos que encontró separados por una coma. Se lee hasta el salto de línea de la fila
+void insertarFilaEnPila(FILE *archivo, Pila *Stack){
+	int codigo=1;
+	char letra;
+	//Mientras no se llegue al final de la fila (Al salto de línea) metemos todos los 
+	//caracteres a la pila
+	while(codigo!=10){
+			letra=fgetc(archivo);
+			codigo=letra;
+			//Se insertan todos los carecteres a la pila excepto
+			//los espacios y los saltos de línea
+			if(codigo!=32 && codigo!=10){
+				apilar(Stack, letra);
+			}			
+	}
+}
+
+void IntercambiarPilas(Pila *Stack1, Pila *Stack2){
+	char letra;
+	int codigo, tamEstado=0, x=0;
+	while(es_vaciaPila(Stack1)!=1){
+		letra=desapilar(Stack1);
+		codigo=letra;
+		apilar(Stack2, letra);
+	}
 }
