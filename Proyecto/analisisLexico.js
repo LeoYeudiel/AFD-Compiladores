6005194ef09e7fed34f27cc7ff53445e455b6a07
LeoYeudiel <lyeuprsw@gmail.com>
2024-06-16 00:11:46 -0600

    Añadí el análisis léxico del proyecto final

---
+++ Proyecto/analisisLexico.js
@@ -0,0 +1,197 @@
+//Definición de Tokens
+const TokenType = {
+  //Palabras reservadas
+    //Cabecera y final del algoritmo
+  ALGORITMO: 'ALGORITMO',
+  FINALGORITMO: 'FINALGORITMO',
+    
+    //Declaración de variables
+  DEFINIR: 'DEFINIR',
+  COMO: 'COMO',
+
+    //Funciones nativas para lectura y escritura
+  LEER: 'LEER',
+  ESCRIBIR: 'ESCRIBIR',
+
+    //Para la declaración de una función
+  FUNCION: 'FUNCION',
+  FINFUNCION: 'FINFUNCION',
+  RETORNAR: 'RETORNAR',
+
+    //Estructura condicional If
+  SI: 'SI',
+  ENTONCES: 'ENTONCES',
+  SINO: 'SINO',
+  FINSI: 'FINSI',
+
+    //Estructura ciclicas while o for
+  MIENTRAS: 'MIENTRAS',
+  HACER: 'HACER',
+  FINMIENTRAS: 'FINMIENTRAS',
+  PARA: 'PARA',
+  HASTA: 'HASTA',
+  CON: 'CON',
+  PASO: 'PASO',
+  FINPARA: 'FINPARA',
+
+    //Valores lógicos
+  VERDADERO: 'VERDADERO',
+  FALSO: 'FALSO',
+  
+    //Tipo de datos
+  ENTERO: 'ENTERO',
+  REAL: 'REAL',
+  CADENA: 'CADENA',
+  LOGICO: 'LOGICO',
+
+    //Operadores lógicos
+  Y: 'Y',
+  O: 'O',
+  NO: 'NO',
+  
+  //Identificadores y literales
+  IDENTIFICADOR: 'IDENTIFICADOR',  // Para identificar cualquier otro identificador
+  NUMERO: 'NUMBER',  // Para números
+  STRING: 'STRING',  // Para cadenas de texto
+
+  //Operadores
+  SUMA: 'SUMA',
+  RESTA: 'RESTA',
+  MULTIPLICACION: 'MULTIPLICATION',
+  DIVISION: 'DIVISION',
+
+  //Delimitadores
+  IZQPAREN: 'IZQPAREN', //(
+  RIGPAREN: 'RIGPAREN', //)
+  ASIGNACION: 'ASIGNACION', //=
+  COMA: 'COMA', //,
+
+  //Fin de línea o Input
+  SALTO: 'SALTO', //Fin de la línea
+  EOF: 'EOF', //Fin de la línea
+}
+
+const TokenPatterns = [
+  { type: TokenType.ALGORITMO, pattern: /^Algoritmo\b/ },
+  { type: TokenType.DEFINIR, pattern: /^Definir\b/ },
+  { type: TokenType.LEER, pattern: /^Leer\b/ },
+  { type: TokenType.ESCRIBIR, pattern: /^Escribir\b/ },
+  { type: TokenType.FINALGORITMO, pattern: /^FinAlgoritmo\b/ },
+  { type: TokenType.COMO, pattern: /^Como\b/ },
+  { type: TokenType.ENTERO, pattern: /^Entero\b/ },
+  { type: TokenType.REAL, pattern: /^Real\b/ },
+  { type: TokenType.CADENA, pattern: /^Cadena\b/ },
+  { type: TokenType.LOGICO, pattern: /^Logico\b/ },
+  { type: TokenType.FUNCION, pattern: /^Funcion\b/ },
+  { type: TokenType.FINFUNCION, pattern: /^FinFuncion\b/ },
+  { type: TokenType.SI, pattern: /^Si\b/ },
+  { type: TokenType.ENTONCES, pattern: /^Entonces\b/ },
+  { type: TokenType.SINO, pattern: /^Sino\b/ },
+  { type: TokenType.FINSI, pattern: /^FinSi\b/ },
+  { type: TokenType.MIENTRAS, pattern: /^Mientras\b/ },
+  { type: TokenType.HACER, pattern: /^Hacer\b/ },
+  { type: TokenType.FINMIENTRAS, pattern: /^FinMientras\b/ },
+  { type: TokenType.PARA, pattern: /^Para\b/ },
+  { type: TokenType.HASTA, pattern: /^Hasta\b/ },
+  { type: TokenType.CON, pattern: /^Con\b/ },
+  { type: TokenType.PASO, pattern: /^Paso\b/ },
+  { type: TokenType.FINPARA, pattern: /^FinPara\b/ },
+  { type: TokenType.RETORNAR, pattern: /^Retornar\b/ },
+  { type: TokenType.VERDADERO, pattern: /^Verdadero\b/ },
+  { type: TokenType.FALSO, pattern: /^Falso\b/ },
+  { type: TokenType.Y, pattern: /^Y\b/ },
+  { type: TokenType.O, pattern: /^O\b/ },
+  { type: TokenType.NO, pattern: /^No\b/ },
+  { type: TokenType.SUMA, pattern: /^\+\b/},
+  { type: TokenType.RESTA, pattern: /^\-\b/},
+  { type: TokenType.MULTIPLICACION, pattern: /^\*\b/},
+  { type: TokenType.DIVISION, pattern: /^\/\b/},
+  { type: TokenType.IZQPAREN, pattern: /^\(\b/},
+  { type: TokenType.RIGPAREN, pattern: /^\)\b/ },
+  { type: TokenType.COMA, pattern: /^,/ },
+  { type: TokenType.SALTO, pattern: /^;/ },
+  { type: TokenType.IDENTIFICADOR, pattern: /^[a-zA-Z_]\w*\b/ },  // Para otros identificadores
+  { type: TokenType.NUMERO, pattern: /^\d+(\.\d+)?\b/ },  // Para números
+  { type: TokenType.STRING, pattern: /"([^"\\]|\\.)*"/ },  // Para cadenas de texto
+]
+
+
+class Token{
+  constructor(type, value, line) {
+    this.type = type;
+    this.value = value;
+    this.line = line
+  }
+
+  toString() { 
+    return `Token (${this.type}, ${this.value}, ${this.line})`;
+  }
+}
+
+
+
+//Análizador léxico
+class Lexer {
+  constructor(input) {
+    this.input = input;
+    this.pos = 0;
+    this.line = 0;
+  }
+
+  advance() {
+    this.pos++;
+    if (this.pos >= this.input.length) {
+      this.currentChar = null; // End of input
+    } else {
+      this.currentChar = this.input[this.pos];
+    }
+  }
+
+  skipWhitespace() {
+    while (this.currentChar !== null && /\s/.test(this.currentChar)) {
+      this.advance();
+    }
+  }
+
+  getNextToken() {
+    if (this.pos >= this.input.length) {
+      return new Token(TokenType.EOF, null);
+    }
+    this.currentChar = this.input[this.pos];
+    this.skipWhitespace();
+    const remainingInput = this.input.slice(this.pos);
+    let value;
+    for (const { type, pattern } of TokenPatterns) {
+      const match = remainingInput.match(pattern);
+      if (match) {
+        value = match[0];
+        this.pos += value.length;
+        const newToken = new Token(type, value, this.line);
+        if (type == "SALTO")
+          this.line++;
+        return newToken;
+      }
+    }
+    return new Token("UNDEFINED", value);
+  }
+
+}
+
+//Prueba
+const input = `Algoritmo prueba;
+Definir x Como Entero;
+Leer x;
+Escribir x;
+FinAlgoritmo;`;
+const lexer = new Lexer(input);
+
+let tokens = [];
+let token;
+do {
+  token = lexer.getNextToken();
+  tokens.push(token);
+} while (token.type !== TokenType.EOF);
+
+console.log(tokens)
+
+
